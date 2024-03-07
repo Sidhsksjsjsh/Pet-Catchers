918d8b206699f2734a96ee8970235afc93d73acd
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-03-07 19:15:18 +0900

    Update .lua

--- .lua
+++ .lua
@@ -7,7 +7,7 @@ T1:Toggle("Auto catch all pets",false,function(value)
     _G.cpets = value
     while wait() do
       if _G.cpets == false then break end
-      for i,v in pairs(workspace["Rendered"]["Pets"]["World"]) do
+      for i,v in pairs(workspace["Rendered"]["Pets"]["World"]:GetChildren()) do
         game:GetService("ReplicatedStorage")["Shared"]["Framework"]["Network"]["Remote"]["Function"]:InvokeServer("CapturePet",v.Name,"Common")
       end
     end
